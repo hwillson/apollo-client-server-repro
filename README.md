55e285c90dff7a74496fc5819783abea52f84193
Hugh Willson <hugh@octonary.com>
2019-04-18 14:54:47 -0400

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # apollo-client-server-repro
-Super simple Apollo Client + Server blank repro template
+
+Super simple Apollo Client + Server blank repro template.
+
+- `./client`: Empty `create-react-app` app with default Apollo Client deps.
+- `./server`: Basic Apollo Server instance. 
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (root, args, context) => 'Hello world!'
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
+
